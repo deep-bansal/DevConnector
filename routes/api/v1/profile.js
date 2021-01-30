799dbff9ab870e8af0fc36f229aab1515ffe36d2
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-30 15:16:10 +0530

    fetch user details route

--- routes/api/v1/profile.js
+++ routes/api/v1/profile.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("Profile Route");
+const auth = require("../../../middlewares/auth");
+const Profile = require("../../../models/Profile");
+const User = require("../../../models/User");
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no Profile for this user" });
+    }
+
+    return res.json(profile);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Server Error");
+  }
 });
 module.exports = router;
