352f7c2f5df54bdafea9c2748fbb92419fb532d2
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-30 19:44:50 +0530

    create and fetch posts

--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -1,6 +1,60 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("Post Route");
+const auth = require("../../../middlewares/auth");
+const User = require("../../../models/User");
+const Post = require("../../../models/Post");
+
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      return res.status(200).json(post);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+router.get("/all", auth, async (req, res) => {
+  try {
+    const posts = await Post.find();
+    return res.status(200).json(posts);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    return res.status(200).json(post);
+  } catch (err) {
+    console.log(err);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 module.exports = router;
