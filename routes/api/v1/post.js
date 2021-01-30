660fc94146b6713ec7032f4ad1cd7809aab8e3c9
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-30 19:55:08 +0530

    delete post

--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -57,4 +57,24 @@ router.get("/:id", auth, async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+    await post.remove();
+    return res.status(200).json({ msg: "Post Removed" });
+  } catch (err) {
+    console.log(err);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 module.exports = router;
