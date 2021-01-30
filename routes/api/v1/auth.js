9d5758d5028259fda89925794d4fa5d6768f31fb
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-30 12:45:41 +0530

    verify user using middleware and return it

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("Auth Route");
+const auth = require("../../../middlewares/auth");
+const User = require("../../../models/User");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
 });
 module.exports = router;
