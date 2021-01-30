adb110050804b5a1e2087e90bba204f977a66755
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-30 10:54:13 +0530

    initial setup and User schema

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const db = require("./config/mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
-
+db();
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+app.use("/api/v1/users", require("./routes/api/v1/users"));
+app.use("/api/v1/profile", require("./routes/api/v1/profile"));
+app.use("/api/v1/post", require("./routes/api/v1/post"));
+app.use("/api/v1/auth", require("./routes/api/v1/auth"));
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
