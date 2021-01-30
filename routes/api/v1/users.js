ffa052bd4692e7d51588057f30760ff888bf7211
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-30 12:20:43 +0530

    user register and token generation

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../../../models/User");
 
 router.post(
   "/",
@@ -14,14 +20,60 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("User Route");
+    const { name, email, password } = req.body;
+    try {
+      //check if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).send("User already Exist");
+      }
+
+      //get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        password,
+        avatar,
+      });
+
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      //return jsonwebtoken
+      const payLoad = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payLoad,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send({ errors: [{ msg: "User already exists" }] });
+    }
   }
 );
 module.exports = router;
