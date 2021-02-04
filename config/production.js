1e4a29ebf249fe36df56b10614e0884207f4c153
deep-bansal <deepvinaybansal@yahoo.com>
2021-02-04 21:50:43 +0530

    error

--- config/production.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: process.env.DEV_MONGO_URI,
-  jwtSecret: process.env.DEV_JWT_SECRET,
-  githubClientSecret: process.env.DEV_CLIENT_SECRET,
-  githubClientId: process.env.DEV_CLIENT_ID,
-};
