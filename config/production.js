39f79df76f9a3ee5fb011a14a117b813ff9c8457
deep-bansal <deepvinaybansal@yahoo.com>
2021-02-04 21:32:43 +0530

    final

---
+++ config/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.DEV_MONGO_URI,
+  jwtSecret: process.env.DEV_JWT_SECRET,
+  githubClientSecret: process.env.DEV_CLIENT_SECRET,
+  githubClientId: process.env.DEV_CLIENT_ID,
+};
